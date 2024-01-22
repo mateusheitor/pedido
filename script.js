25f6e34f22575474c84afaeefa084746d7686ef4
Heitor <127308394+mateusheitor@users.noreply.github.com>
2024-01-22 14:41:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var yesBtn = document.getElementById('yesBtn');
+    var noBtn = document.getElementById('noBtn');
+  
+    yesBtn.addEventListener('click', function() {
+      alert("olha pra trás <3 ");
+    });
+  
+    noBtn.addEventListener('click', function() {
+      alert("Tudo bem. Eu entendo. Você é uma pessoa incrível e por você vale apena esperar.");
+    });
+  };
+  
